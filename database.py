683fa0a27a362a8d5f437ca19e33b1cf7ab90f77
Ralph Hung <hung5422@tdi.edu.tw>
2023-05-04 16:10:31 +0800

    removed SECRETSTRING in database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# db_connection_string = "mysql+pymysql://i5r94kko1t9jhkilp2k0:pscale_pw_J6iFbowzqC4nhSmF419YofiTukp58XSpO7QiXKTovPd@aws.connect.psdb.cloud/flask1?charset=utf8mb4"
 db_connection_string = os.environ['SECRETSTRING']
 
 engine = create_engine(
